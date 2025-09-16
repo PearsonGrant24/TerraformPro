6f9a436ab52ef7656dbf8eae8644b106c8dae3b9
Pearson Grant <pearsongrant23@gmail.com>
2025-09-16 15:05:01 +0200

    create index in s3

--- Modules/s3/main.tf
+++ Modules/s3/main.tf
@@ -10,6 +10,14 @@ resource "aws_s3_bucket_website_configuration" "website" {
     bucket = aws_s3_bucket.s3.bucket
 
     index_document {
-      suffix = "/index.html"
+      suffix = "index.html"
     }  
 }
+
+# resource "aws_s3_object" "index" {
+#   bucket = aws_s3_bucket.s3.bucket
+#   key    = "index.html"
+#   source = "/index.html"
+#   acl    = "public-read"
+#   content_type = "text/html"
+# }
