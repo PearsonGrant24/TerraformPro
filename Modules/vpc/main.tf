83ddc1fd78698a5077182e5fd73fcf64d6be2220
Pearson Grant <pearsongrant23@gmail.com>
2025-09-23 22:46:48 +0200

    rewrite

--- Modules/vpc/main.tf
+++ Modules/vpc/main.tf
@@ -1,96 +1,118 @@
+
 resource "aws_vpc" "main" {
-  cidr_block           = var.cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block = var.cidr_block
 
   tags = {
-    Name = "${var.project_name}-vpc"
+    "name" = "pract-vpc"
   }
 }
 
-# ----------------------
-# Internet Gateway
-# ----------------------
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "subnet" {
+  count = length(var.subnets)
 
-  tags = {
-    Name = "${var.project_name}-igw"
-  }
-}
-
-# ----------------------
-# Public Subnets
-# ----------------------
-resource "aws_subnet" "public" {
-  for_each = { for idx, cidr in var.public_subnets : idx => cidr }
-
-  # count             = length(var.public_subnets)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = each.value
-  availability_zone = var.availability_zones[tonumber(each.key)]
-
-  map_public_ip_on_launch = true
-
-  depends_on = [ aws_vpc.main ]
-
+  cidr_block        = element(var.subnets, count.index)
+  availability_zone = element(var.availabilty_zones, count.index)
+  
   tags = {
-    Name = "${var.project_name}-public-${each.key}"
+    Name = "pract-subnet-${count.index}"
   }
 }
 
-# Route Table for Public Subnets
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+# resource "aws_vpc" "main" {
+#   cidr_block           = var.cidr_block
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${var.project_name}-public-rt"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-vpc"
+#   }
+# }
 
-resource "aws_route_table_association" "public_assoc" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
+# # ----------------------
+# # Internet Gateway
+# # ----------------------
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.main.id
 
-# ----------------------
-# Private Subnets
-# ----------------------
-resource "aws_subnet" "private" {
-  for_each = { for idx, cidr in var.private_subnets : idx => cidr }
+#   tags = {
+#     Name = "${var.project_name}-igw"
+#   }
+# }
 
-  # count             = length(var.private_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = each.value
-  availability_zone = var.availability_zones[tonumber(each.key)]
-  # element(var.availabilty_zones, count.index)
+# # ----------------------
+# # Public Subnets
+# # ----------------------
+# resource "aws_subnet" "public" {
+#   for_each = { for idx, cidr in var.public_subnets : idx => cidr }
 
-  map_public_ip_on_launch = false
+#   # count             = length(var.public_subnets)
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = each.value
+#   availability_zone = var.availability_zones[tonumber(each.key)]
 
-  depends_on = [ aws_vpc.main ]
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "${var.project_name}-private-${each.key}"
-  }
-}
+#   depends_on = [ aws_vpc.main ]
 
-# Private Route Table (no IGW route here unless you add NAT Gateway later)
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+#   tags = {
+#     Name = "${var.project_name}-public-${each.key}"
+#   }
+# }
 
-  tags = {
-    Name = "${var.project_name}-private-rt"
-  }
-}
+# # Route Table for Public Subnets
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-resource "aws_route_table_association" "private_assoc" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+
+#   tags = {
+#     Name = "${var.project_name}-public-rt"
+#   }
+# }
+
+# resource "aws_route_table_association" "public_assoc" {
+#   count          = length(aws_subnet.public)
+#   subnet_id      = aws_subnet.public[count.index].id
+#   route_table_id = aws_route_table.public.id
+# }
+
+# # ----------------------
+# # Private Subnets
+# # ----------------------
+# resource "aws_subnet" "private" {
+#   for_each = { for idx, cidr in var.private_subnets : idx => cidr }
+
+#   # count             = length(var.private_subnets)
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = each.value
+#   availability_zone = var.availability_zones[tonumber(each.key)]
+#   # element(var.availabilty_zones, count.index)
+
+#   map_public_ip_on_launch = false
+
+#   depends_on = [ aws_vpc.main ]
+
+#   tags = {
+#     Name = "${var.project_name}-private-${each.key}"
+#   }
+# }
+
+# # Private Route Table (no IGW route here unless you add NAT Gateway later)
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+
+#   tags = {
+#     Name = "${var.project_name}-private-rt"
+#   }
+# }
+
+# resource "aws_route_table_association" "private_assoc" {
+#   count          = length(aws_subnet.private)
+#   subnet_id      = aws_subnet.private[count.index].id
+#   route_table_id = aws_route_table.private.id
+# }
--- Modules/vpc/outputs.tf
+++ Modules/vpc/outputs.tf
@@ -2,14 +2,19 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
-#   value = aws_subnet.public[*].id  bcz we swithced from count
-description = "List of public subnet IDs"
-  value       = [for subnet in aws_subnet.public : subnet.id]
-}
+# output "public_subnet_ids" {
+# #   value = aws_subnet.public[*].id  bcz we swithced from count
+# description = "List of public subnet IDs"
+#   value       = [for subnet in aws_subnet.public : subnet.id]
+# }
 
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = [for subnet in aws_subnet.private : subnet.id]
+# output "private_subnet_ids" {
+#   description = "List of private subnet IDs"
+#   value       = [for subnet in aws_subnet.private : subnet.id]
+# }
+# # value       = values(aws_subnet.private)[*].id           also works (each)
+
+
+output "subnet_ids" {
+  value = aws_subnet.subnet[*].id
 }
-# value       = values(aws_subnet.private)[*].id           also works (each)
