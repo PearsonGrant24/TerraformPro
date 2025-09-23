bdd5772531244796f47065e8a3328db662fd7e30
Pearson Grant <pearsongrant23@gmail.com>
2025-09-23 21:41:30 +0200

    subnets issues

--- Modules/vpc/outputs.tf
+++ Modules/vpc/outputs.tf
@@ -3,9 +3,13 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+#   value = aws_subnet.public[*].id  bcz we swithced from count
+description = "List of public subnet IDs"
+  value       = [for subnet in aws_subnet.public : subnet.id]
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  description = "List of private subnet IDs"
+  value       = [for subnet in aws_subnet.private : subnet.id]
 }
+# value       = values(aws_subnet.private)[*].id           also works (each)
