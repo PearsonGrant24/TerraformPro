4d0be021365877633ce81ad79d4f5e526fe85774
Pearson Grant <pearsongrant23@gmail.com>
2025-09-23 22:50:43 +0200

    rewrite

--- Envs/dev/outputs.tf
+++ Envs/dev/outputs.tf
@@ -2,36 +2,36 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "public_subnets" {
-  value = module.vpc.public_subnet_ids
-}
+# output "public_subnets" {
+#   value = module.vpc.public_subnet_ids
+# }
 
-output "private_subnets" {
-  value = module.vpc.private_subnet_ids
-}
+# output "private_subnets" {
+#   value = module.vpc.private_subnet_ids
+# }
 
-output "ec2_public_ip" {
-  value = module.ec2.monitor_public_ip
-}
+# output "ec2_public_ip" {
+#   value = module.ec2.monitor_public_ip
+# }
 
 output "ec2_instance_id" {
   value = module.ec2.instance_id
 }
 
-output "grafana" {
+# output "grafana" {
     
-    value = "http://${module.ec2.instance_id}:3000"
-}
+#     value = "http://${module.ec2.instance_id}:3000"
+# }
 
-output "prometheus_url" {
-  value = "http://${module.ec2.instance_id}:9090"
-}
+# output "prometheus_url" {
+#   value = "http://${module.ec2.instance_id}:9090"
+# }
 
 
-output "ec2_vpc_id" {
-  description = "VPC ID used by the EC2 instance"
-  value       = module.ec2.subnet_vpc_id
-}
+# output "ec2_vpc_id" {
+#   description = "VPC ID used by the EC2 instance"
+#   value       = module.ec2.subnet_vpc_id
+# }
 
 # output "monitor_public_ip" {
 #   value = module.ec2.monitor_public_ip
